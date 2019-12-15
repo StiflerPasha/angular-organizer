a6571a38da18806e454db8124310fd8f0c4fc3f0
StiflerPasha <dvoynospavel@gmail.com>
2019-12-15 15:07:56 +0300

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { OrganizerComponent } from './components/organizer/organizer.component';
+import { SelectorComponent } from './components/selector/selector.component';
+import { MomentPipe } from './shared/moment.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    OrganizerComponent,
+    SelectorComponent,
+    MomentPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
